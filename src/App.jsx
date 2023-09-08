1c4abfcbd10bf0776bebf3796ab455ed31e1ef28
F2 <f2@imacs-iMac-2.local>
2023-09-08 12:05:51 +0700

    1st init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './components/Nav'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Nav />
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
